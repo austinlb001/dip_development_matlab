de1169f355428015b5f4e7c048d5b3126f257c28
Katie-dd <66805975+Katie-dd@users.noreply.github.com>
2023-12-01 17:37:39 -0600

    CURE-OR and metrics updates

--- datasets_reading.m
+++ datasets_reading.m
@@ -1,5 +1,8 @@
 classdef datasets_reading
     methods(Static)
+
+%%%%%%%%%%%%%%%%%%%%%% CURE-OR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
         function file_storage=cure_or_paths
             % BACKGROUND: 1:6 (EXCLUDES 6)
             background = [1:6];
@@ -56,6 +59,62 @@ classdef datasets_reading
 
 
         end 
-    end
 
+%%%%%%%%%%%%%%%%%%%%%%%%% CURE - TSR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function file_storage = cure_tsr_paths
+   
+ % sequenceType: 1:3 (EXCLUDES 3)
+    sequenceType = [1: 3];
+
+  % signType: 01:15 (EXCLUDES 15)
+    signType = [1: 15];
+
+  % index: 1:1195
+
+  index = [1:1195];
+
+    file_paths_temp = [];
+            file_paths = [];
+            files_store = [];
+            
+            % File lists
+            root_directory = "C:\Users\Katie\Documents\School\GaTech\Fall 2023 -Digital Image Processing\dippykit\Project_Images\CURE_TSR\Real_Test\Real_Test\";
+            file_list = dir(fullfile(root_directory, '**\*.bmp'));
+
+            % Full file paths
+
+            for ii=1:size(file_list,1)
+            
+                file_name = file_list(ii).name;
+                
+                file_dir = file_list(ii).folder;
+
+                full_path{ii} = fullfile(file_dir,file_name); 
+
+            end 
+
+            iter1 = 0;
+            for idx1 = sequenceType
+                for idx2 = signType
+                    iter2 = 0;
+                    for idx3 = index
+                        [~,name,~] = fileparts(full_path);
+                            file_beginning = strcat(num2str(idx1,'%02.f'),'_',num2str(idx2,'%02.f'));
+                            file_string = contains(full_path,file_beginning) & name(end-3:end)==num2str(index,'%04.f');
+                            if ~all(file_string==0)
+                                iter2 = iter2 + 1;
+                            file_store{iter2} = full_path(file_string);
+                            end
+                    end
+                            iter1 = iter1 + 1;
+                            file_storage{iter1} = file_store;
+                            end 
+                        end 
+                    end 
+end 
+
+   % CHALLENGETYPE: 00 - No challenge; 01 - Decolorization; 02 - Lens blur; 03 - Codec error; 
+        % 04 - Darkening; 05 - Dirty lens; 06 - Exposure; 07 - Gaussian blur; 08 - Noise; 09 - Rain; 10 - Shadow; 11 - Snow; 12 - Haze
+    % CHALLENGELEVEL: NO CHALLENGE 0, 1:5 (INCLUDES 5)
 end
--- iqa_metrics/metrics.m
+++ iqa_metrics/metrics.m
@@ -1,12 +1,48 @@
-function [psnr_value,ssim_value]=metrics(img, original_img)
+function [psnr_value,ssim_value,cw_ssim_value,UNIQUE_value,MS_UNIQUE_value,csv_value, SUMMER_value]=metrics(img, original_img)
+
+% 1. PSNR 
+% 2. SSIM
+% 3. CW-SSIM
+% 4. UNIQUE 
+% 5. MS-UNIQUE
+% 6. CSV
+% 7. SUMMER
+
+
+% image resize 
+
+if size(img,1)~= size(original_img,1)
+    img = imresize(img,[size(original_img,1) size(original_img,2)],"bilinear");
+end
+
 
 % PSNR 
 
 psnr_value = psnr(img,original_img);
 
-
 % SSIM 
 
 ssim_value = ssim(img, original_img);
+
+% CW-SSIM
+
+cw_ssim_value = cwssim_index(img, original_img,4,4,0,0);
+
+% UNIQUE
+
+UNIQUE_value = mslUNIQUE(img, original_img);
+
+% MS-UNIQUE
+
+MS_UNIQUE_value = mslMSUNIQUE(img, original_img);
+
+
+% CSV
+
+csv_value = csv(img, original_img);
+
+% SUMMER
+
+SUMMER_value = SUMMER(original_img,img);
 
 end
