e1a6d9ab046f1e03291bbe31a6694612a2f38aa5
Katie-dd <66805975+Katie-dd@users.noreply.github.com>
2023-12-02 00:30:44 -0600

    TSR and TRD reading improvements

--- datasets_reading.m
+++ datasets_reading.m
@@ -1,31 +1,31 @@
 classdef datasets_reading
     methods(Static)
 
-%%%%%%%%%%%%%%%%%%%%%% CURE-OR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%% CURE-OR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
         function file_storage=cure_or_paths
             % BACKGROUND: 1:5 (INCLUDES 5)
             background = 1:1:5;
-    
+
             % DEVICE TYPE: 1:5 (INCLUDES 5)
             device = 1:1:5;
-    
+
             % OBJECT ORIENTATION: 1:5 (INCLUDES 5)
             objectOrientation = 1:1:5;
-    
+
             % OBJECT TYPE: 000:100 (INCLUDES 100)
             object_type = 1:1:100;
 
             %CHALLENGETYPE: NO CHALLENGE 01, CHALLENGE 02:09 (INCLUDES 09), GRAYSCALE NO CHALLENGE 10, GRAYSCALE 11:18 (INCLUDES 18)
             % CHALLENGELEVEL: NO CHALLENGE 0, 1:5 (INCLUDES 5)
-            
+
             % ASSIGN ROOT DIRECTORY
             if getenv('username') == "Katie"
                 root_directory = "C:\Users\Katie\Documents\School\GaTech\Fall 2023 -Digital Image Processing\dippykit\Project_Images\CURE_OR\";
             elseif getenv("USER") == "austinlb001"
                 root_directory = "/run/media/austinlb001/DATA2/CURE-OR/";
             end
-           
+
             % GET ALL FILES
             if ispc
                 file_list = dir(fullfile(root_directory, '**\*.jpg'));
@@ -36,16 +36,16 @@ classdef datasets_reading
             % CONSTRUCT FILE PATHS
             full_path = cell(size(file_list));
             parfor ii=1:size(file_list,1)
-                full_path{ii} = fullfile(file_list(ii).folder, file_list(ii).name); 
-            end 
-            
+                full_path{ii} = fullfile(file_list(ii).folder, file_list(ii).name);
+            end
+
             % SORT FILE PATHS
             iter1 = 0;
-            for idx1=background 
-                for idx2=device 
+            for idx1=background
+                for idx2=device
                     for idx3=objectOrientation
                         iter2 = 0;
-                        for idx4=object_type 
+                        for idx4=object_type
                             file_beginning = strcat(num2str(idx1),'_',num2str(idx2),'_',num2str(idx3),'_',num2str(idx4,'%03.f'));
                             file_string = contains(full_path,file_beginning);
                             if ~all(file_string==0)
@@ -58,45 +58,45 @@ classdef datasets_reading
                     end
                 end
             end
-        end 
+        end
 
-%%%%%%%%%%%%%%%%%%%%%%%%% CURE - TSR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%% CURE - TSR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
         function file_storage = cure_tsr_paths
-       
+
             % sequenceType: 1:2 (INCLUDES 2)
             sequenceType = 1:1:2;
-        
+
             % signType: 01:14 (INCLUDES 14)
             signType = 1:1:14;
 
-            % CHALLENGETYPE: 00 - No challenge; 01 - Decolorization; 02 - Lens blur; 03 - Codec error; 
+            % CHALLENGETYPE: 00 - No challenge; 01 - Decolorization; 02 - Lens blur; 03 - Codec error;
             % 04 - Darkening; 05 - Dirty lens; 06 - Exposure; 07 - Gaussian blur; 08 - Noise; 09 - Rain; 10 - Shadow; 11 - Snow; 12 - Haze
             % CHALLENGELEVEL: NO CHALLENGE 0, 1:5 (INCLUDES 5)
-        
+
             % index: 1:1195
             index = 1:1:1195;
-                    
+
             % ASSIGN ROOT DIRECTORY
             if getenv('username') == "Katie"
                 root_directory = "C:\Users\Katie\Documents\School\GaTech\Fall 2023 -Digital Image Processing\dippykit\Project_Images\CURE_TSR\Real_Test\Real_Test\";
             elseif getenv("USER") == "austinlb001"
                 root_directory = "/run/media/austinlb001/DATA2/CURE-TSR/test/Real_Test/";
             end
-            
+
             % GET ALL FILES
             if ispc
                 file_list = dir(fullfile(root_directory, '**\*.jpg'));
             elseif isunix
                 file_list = dir(fullfile(root_directory, '**/*.jpg'));
             end
-            
+
             % CONSTRUCT FILE PATHS
             full_path = cell(size(file_list));
             for ii=1:size(file_list,1)
-                full_path{ii} = fullfile(file_list(ii).folder, file_list(ii).name); 
-            end 
-            
+                full_path{ii} = fullfile(file_list(ii).folder, file_list(ii).name);
+            end
+
             % SORT FILES
             iter1 = 0;
             for idx1 = sequenceType
@@ -106,87 +106,79 @@ classdef datasets_reading
                     for idx3 = index
 
                         disp(idx3)
-                            file_beginning = num2str(idx1,'%02.f') + "_" + num2str(idx2,'%02.f') ; 
-                            pat = file_beginning + "_" + digitsPattern(2) + "_" +digitsPattern(2) +"_" + num2str(idx3,'%04.f');
-                            file_string = contains(full_path,pat);
-                            
-                            if ~all(file_string==0)
-                                iter2 = iter2 +1;  
+                        file_beginning = num2str(idx1,'%02.f') + "_" + num2str(idx2,'%02.f') ;
+                        pat = file_beginning + "_" + digitsPattern(2) + "_" +digitsPattern(2) +"_" + num2str(idx3,'%04.f');
+                        file_string = contains(full_path,pat);
+
+                        if ~all(file_string==0)
+                            iter2 = iter2 +1;
                             file_store{iter2} = full_path(file_string);
-                            end
-                            
-                            iter1 = iter1 + 1;
-                            file_storage{iter1} = file_store;
-                            end 
-                        end 
-                    end 
-end 
+                        end
 
-   % CHALLENGETYPE: 00 - No challenge; 01 - Decolorization; 02 - Lens blur; 03 - Codec error; 
+                       
+                    end
+                 iter1 = iter1 + 1;
+                        file_storage{iter1} = file_store;
+                end
+            end
+        end
+
+        % CHALLENGETYPE: 00 - No challenge; 01 - Decolorization; 02 - Lens blur; 03 - Codec error;
         % 04 - Darkening; 05 - Dirty lens; 06 - Exposure; 07 - Gaussian blur; 08 - Noise; 09 - Rain; 10 - Shadow; 11 - Snow; 12 - Haze
-    % CHALLENGELEVEL: NO CHALLENGE 0, 1:5 (INCLUDES 5)
+        % CHALLENGELEVEL: NO CHALLENGE 0, 1:5 (INCLUDES 5)
 
 
 
 
-%%%%%%%%%%%%%%%%%%%%% CURE - TSD %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%% CURE - TSD %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-function file_storage = cure_tsd_paths
+        function file_storage = cure_tsd_paths
 
-    % sequenceType: 1:3 (EXCLUDES 3)
-    sequenceType = [1: 2];
+            % sequenceType: 1:3 (EXCLUDES 3)
+            sequenceType = [1: 2];
 
-  % signType: 01:15 (EXCLUDES 15)
-    sequenceNumber = [1: 49];
+            % signType: 01:15 (EXCLUDES 15)
+            sequenceNumber = [1: 49];
 
 
             file_paths_temp = [];
             file_paths = [];
             files_store = [];
-            
+
             % File lists
             root_directory = "C:\Users\Katie\Documents\School\GaTech\Fall 2023 -Digital Image Processing\dippykit\Project_Images\CURE_TSR\Real_Test\Real_Test\";
             file_list = dir(fullfile(root_directory, '**\*.mp4'));
- 
+
             % Full file paths
 
             for ii=1:size(file_list,1)
-            
+
                 file_name = file_list(ii).name;
-                
+
                 file_dir = file_list(ii).folder;
 
-                full_path{ii} = fullfile(file_dir,file_name); 
+                full_path{ii} = fullfile(file_dir,file_name);
 
-            end 
+            end
 
             iter1 = 0;
             for idx1 = sequenceType
                 iter2 = 0;
                 for idx2 = sequenceNumber
-                     file_beginning = strcat(num2str(idx1,'%02.f'),'_',num2str(idx2,'%02.f'));
-                            file_string = contains(full_path,file_beginning);
-                            if ~all(file_string==0)
-                                iter2 = iter2 + 1;
-                            file_store{iter2} = full_path(file_string);
-                            end
+                    file_beginning = strcat(num2str(idx1,'%02.f'),'_',num2str(idx2,'%02.f'));
+                    file_string = contains(full_path,file_beginning);
+                    if ~all(file_string==0)
+                        iter2 = iter2 + 1;
+                        file_store{iter2} = full_path(file_string);
+                    end
                 end
+            iter1 = iter1 + 1;
+            file_storage{iter1} = file_store;
             end
 
+        end
+        
     end
-                        [~,name,~] = fileparts(full_path);
-                            file_beginning = strcat(num2str(idx1,'%02.f'),'_',num2str(idx2,'%02.f'));
-                            file_string = contains(full_path,file_beginning) & name(end-3:end)==num2str(index,'%04.f');
-                            if ~all(file_string==0)
-                                iter2 = iter2 + 1;
-                                file_store{iter2} = full_path(file_string);
-                            end
-                    end
-                    iter1 = iter1 + 1;
-                    file_storage{iter1} = file_store;
-                 end 
-             end 
-        end 
-    end
+    
 end
